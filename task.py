892b2ae60c205bbfc4feb64319f8e4f5091b2577
Igor <oligor2@yandex.ru>
2022-12-06 11:00:44 +0500

    task

--- task.py
+++ task.py
@@ -112,3 +112,24 @@
 # fibo_nums = get_fibonacci(n)
 # print(get_fibonacci(n))
 # print(fibo_nums.index(0))
+
+# 1. Задана натуральная степень k. Сформировать случайным образом список 
+# коэффициентов (значения от 0 до 100) многочлена 
+# и записать в файл многочлен степени k.
+
+# from random import randint
+# max_val=100
+# k = int(input('Введите натуральную степень k:'))
+# # коэфф. при старшей степени не должен быть равен 0
+# koeff=[randint(0,max_val) for i in range(k)]+[randint(1,max_val)]
+# poly='+'.join([f'{(j,"")[j==1]}x^{i}' for i, j in enumerate(koeff) if j][::-1])
+# # Поиск и замены:
+# poly=poly.replace('x^1+', 'x+')
+# poly=poly.replace('x^0', '')
+# poly+=('','1')[poly[-1]=='+']
+# poly=(poly, poly[:-2])[poly[-2:]=='^1']
+# print(poly)
+
+# 2. Даны два файла, в каждом из которых находится запись многочлена.
+# Задача - сформировать файл, содержащий сумму многочленов.
+
