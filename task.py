ca410f7faaf9d3669743707f004f434a3910bd67
Igor <oligor2@yandex.ru>
2022-12-10 10:20:57 +0500

    task

--- task.py
+++ task.py
@@ -133,3 +133,138 @@
 # 2. Даны два файла, в каждом из которых находится запись многочлена.
 # Задача - сформировать файл, содержащий сумму многочленов.
 
+# 1. Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+
+# txt = input("Введите текст через пробел:\n")
+# print(f"Исходный текст: {txt}")
+# find_txt = "абв"
+# lst = [i for i in txt.split() if find_txt not in i]
+# print(f'Результат: {" ".join(lst)}')
+
+# 2. Создайте программу для игры с конфетами человек против человека.
+
+# Условие задачи: На столе лежит 2021 конфета. 
+# Играют два игрока делая ход друг после друга.
+# Первый ход определяется жеребьёвкой.
+# За один ход можно забрать не более чем 28 конфет. 
+# Все конфеты оппонента достаются сделавшему последний ход.
+# Сколько конфет нужно взять первому игроку,
+# чтобы забрать все конфеты у своего конкурента?
+
+# a) Добавьте игру против бота
+
+# b) Подумайте как наделить бота ""интеллектом""
+
+
+# 3. Создайте программу для игры в ""Крестики-нолики"".
+
+
+# print('*'*100)
+# print('\n')
+# print('А теперь давайте сыграем в крестики нолики!')
+
+# board = list(range(1,10))
+
+# def design_board(board):
+#     print('-'*12)
+#     for i in range(3):
+#         print('|', board[0+i*3],'|', board[1+i*3], '|', board[2+i*3], '|')
+#         print('-'*12)
+
+# # design_board(board)
+
+# def choice(tic_tac):
+#     valid = False
+#     while not valid:
+#         player_index = input('Ваш ход, выберите ячейку ' + tic_tac + ' --> ')
+#         try:
+#             player_index =int(player_index)
+#         except:
+#             print('Что то не то нажали')
+#             continue
+#         if player_index >= 1 and player_index <= 9:
+#             if(str(board[player_index-1]) not in 'XO'):
+#                 board[player_index-1] = tic_tac
+#                 valid = True
+#             else:
+#                 print('Занято')
+#         else:
+#             print('Еще раз попробуй')
+
+# def victory_check(board):
+#     victory = ((0,1,2),(3,4,5),(6,7,8),
+#                (0,3,6),(1,4,7),(2,5,8),
+#                (0,4,8),(2,4,6))
+#     for i in victory:
+#         if board[i[0]] == board[i[1]] == board[i[2]]:
+#             return board[i[0]]
+#     return False
+
+# def game(board):
+#     counter =0
+#     vic = False
+#     while not vic:
+#         design_board(board)
+#         if counter % 2 == 0:
+#             choice('X')
+#         else:
+#             choice('0')
+#         counter +=1
+#         if counter > 4:
+#             tt_win = victory_check(board)
+#             if tt_win:
+#                 print(tt_win,'Победа')
+#                 vic = True
+#                 break
+#             if counter == 9:
+#                 print('Победила, ДРУЖБА)')
+#         design_board(board)
+# game(board)
+
+# 4. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+
+# with open('file_encode.txt', 'w') as data:
+#     data.write('WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW')
+
+# with open('file_encode.txt', 'r') as data:
+#     string = data.readline()
+
+# def rle_encode(decoded_string):
+#     encoded_string = ''
+#     count = 1
+#     char = decoded_string[0]
+#     for i in range(1, len(decoded_string)):
+#         if decoded_string[i] == char:
+#             count += 1
+#         else:
+#             encoded_string = encoded_string + str(count) + char
+#             char = decoded_string[i]
+#             count = 1
+#             encoded_string = encoded_string + str(count) + char
+#     return encoded_string
+
+
+# def rle_decode(encoded_string):
+#     decoded_string = ''
+#     char_amount = ''
+#     for i in range(len(encoded_string)):
+#         if encoded_string[i].isdigit():
+#             char_amount += encoded_string[i]
+#         else:
+#             decoded_string += encoded_string[i] * int(char_amount)
+#         char_amount = ''
+#     print(decoded_string)
+
+#     return decoded_string
+
+
+# with open('file_encode.txt', 'r') as file:
+#     decoded_string = file.read()
+
+# with open('file_decode.txt', 'w') as file:
+#     encoded_string = rle_encode(decoded_string)
+#     file.write(encoded_string)
+
+# print('Decoded string: \t' + decoded_string)
+# print('Encoded string: \t' + rle_encode(decoded_string))
+# print(f'Compress ratio: \t{round(len(decoded_string) / len(encoded_string), 1)}')
